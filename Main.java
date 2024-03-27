ac3f3dffe7cf86007cf4a8b30eadd77f806f57f7
Curtis-Edwards <99354904+Curtis-Edwards@users.noreply.github.com>
2024-03-27 16:28:19 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,45 +1,45 @@
 /*
-* This is the standard Main.java starting point.
-*
-* @author  Curtis Edwards
-* @version 1.0
-* @since   2024-03-27
-*/
+ * This is the standard Main.java starting point.
+ *
+ * @author  Curtis Edwards
+ * @version 1.0
+ * @since   2024-03-27
+ */
 
 /**
-* This is the Main program.
-*/
+ * This is the Main program.
+ */
 final class Main {
     /**
-    * Prevent instantiation
-    * Throw an exception IllegalStateException.
-    * if this ever is called
-    *
-    * @throws IllegalStateException if instantiation is attempted
-    */
+     * Prevent instantiation.
+     * Throw an exception IllegalStateException.
+     * if this ever is called.
+     *
+     * @throws IllegalStateException if instantiation is attempted
+     */
     private Main() {
         throw new IllegalStateException("Cannot be instantiated");
     }
 
     /**
-    * The starting main() function.
-    *
-    * @param args No args will be used
-    */
+     * The starting main() function.
+     *
+     * @param args No args will be used
+     */
     public static void main(String[] args) {
-        final CurtisStack<String> fruits = new CurtisStack<>();
+        final CurtisStack fruits = new CurtisStack();
         fruits.push("apple");
         fruits.push("banana");
         fruits.push("cherry");
         System.out.println("fruits: " + fruits.showStack());
 
-        final CurtisStack<String> colours = new CurtisStack<>();
+        final CurtisStack colours = new CurtisStack();
         colours.push("red");
         colours.push("green");
         colours.push("blue");
         System.out.println("colours: " + colours.showStack());
 
-        final CurtisStack<String> shapes = new CurtisStack<>();
+        final CurtisStack shapes = new CurtisStack();
         shapes.push("square");
         shapes.push("triangle");
         shapes.push("circle");
