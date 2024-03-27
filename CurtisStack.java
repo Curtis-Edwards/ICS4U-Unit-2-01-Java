221eb3ce595a505542c4c209b4dbb0ace225ca55
Curtis-Edwards <99354904+Curtis-Edwards@users.noreply.github.com>
2024-03-27 16:25:03 -0400

    Update CurtisStack.java

--- CurtisStack.java
+++ CurtisStack.java
@@ -1,46 +1,44 @@
 /*
-* The CurtisStack Class.
-*
-* @author  Curtis Edwards
-* @version 1.0
-* @since   2024-03-27
-*/
+ * The CurtisStack Class.
+ *
+ * @author  Curtis Edwards
+ * @version 1.0
+ * @since   2024-03-27
+ */
 
 import java.util.ArrayList;
 import java.util.List;
 
 /**
-* The CurtisStack() class.
-*
-* @param <T> as the type parameter
-*/
-public class CurtisStack<T> {
+ * The CurtisStack class.
+ */
+public class CurtisStack {
     /**
-    * Declares a private instance variable
-    */
-    private List<T> stackAsList;
+     * Declares a private instance variable.
+     */
+    private List<Object> stackAsList;
 
     /**
-    * The constructor().
-    */
+     * The constructor.
+     */
     public CurtisStack() {
         stackAsList = new ArrayList<>();
     }
 
     /**
-    * The push() method.
-    *
-    * @param input from Main.java
-    */
-    public void push(T input) {
+     * The push method.
+     *
+     * @param input from Main.java
+     */
+    public void push(Object input) {
         stackAsList.add(input);
     }
 
     /**
-    * The showStack() method.
-    *
-    * @return the values as a string
-    */
+     * The showStack method.
+     *
+     * @return the values as a string
+     */
     public String showStack() {
         final StringBuilder values = new StringBuilder();
         for (int counter = 0; counter < stackAsList.size(); counter++) {
@@ -52,4 +50,3 @@ public class CurtisStack<T> {
         return values.toString();
     }
 }
-
